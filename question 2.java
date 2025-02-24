89d2b11a08bdd9d3b8ed314af54225770159c35a
AnshulGaurwebd <159431971+AnshulGaurwebd@users.noreply.github.com>
2025-02-24 19:49:17 +0530

    Update question 2.java

--- question 2.java
+++ question 2.java
@@ -1,7 +1,6 @@
 //22bcs13276_Anshul Gaur
 
 import java.io.*;
-import java.util.*;
 
 class Student implements Serializable {
     private int id;
@@ -21,29 +20,25 @@ class Student implements Serializable {
 
 public class Main {
     public static void main(String[] args) {
-        List<String> numbers = Arrays.asList("10", "20", "30", "40", "50");
-        List<Integer> intList = new ArrayList<>();
-        for (String num : numbers) {
-            intList.add(Integer.parseInt(num));
-        }
-        int sum = 0;
-        for (Integer num : intList) {
-            sum += num;
-        }
-        System.out.println("Sum: " + sum);
-        
-        Student student = new Student(1, "Evelyn", 3.9);
         String filename = "student.ser";
+        Student student = new Student(1, "Evelyn", 3.9);
+        serializeStudent(student, filename);
+        deserializeStudent(filename);
+    }
+
+    private static void serializeStudent(Student student, String filename) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
             out.writeObject(student);
             System.out.println("Serialization Successful");
         } catch (IOException e) {
             System.out.println("Error during serialization: " + e);
         }
-        
+    }
+
+    private static void deserializeStudent(String filename) {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
-            Student deserializedStudent = (Student) in.readObject();
-            System.out.println("Deserialized Student: " + deserializedStudent);
+            Student student = (Student) in.readObject();
+            System.out.println("Deserialized Student: " + student);
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e);
         } catch (IOException e) {
